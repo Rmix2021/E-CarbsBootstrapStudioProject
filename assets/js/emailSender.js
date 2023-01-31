de30806d8c2607de05f51f136104d93a5376f1f6
Rmix2021 <eurotuner1981@gmail.com>
2023-01-31 14:21:29 -0600

    InitalCommitFromBSS

---
+++ assets/js/emailSender.js
@@ -0,0 +1,5 @@
+<form method="Post" action="http://e85carbs.com/robmix@e85carbs.com">
+    <input type="email" name="email" placeholder="Your Email">
+        <textareaname="message" placeholder="Your message"></textarea>
+<buttontype=" submit"> Send</button>
+</form>
